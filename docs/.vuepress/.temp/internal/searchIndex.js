9c53f2caa6bbea48a0ae18f0975f2a84f397ce0f
nikba <41567806+Nikba-Creative-Studio@users.noreply.github.com>
2024-03-09 07:58:36 +0200

    Style Update

--- docs/.vuepress/.temp/internal/searchIndex.js
+++ docs/.vuepress/.temp/internal/searchIndex.js
@@ -261,6 +261,35 @@ export const searchIndex = [
   {
     "title": "Introducere în curs",
     "headers": [],
+    "path": "/guide/",
+    "pathLocale": "/",
+    "extraFields": []
+  },
+  {
+    "title": "Using Vue in Markdown",
+    "headers": [
+      {
+        "level": 2,
+        "title": "Browser API Access Restrictions",
+        "slug": "browser-api-access-restrictions",
+        "link": "#browser-api-access-restrictions",
+        "children": []
+      },
+      {
+        "level": 2,
+        "title": "",
+        "slug": "",
+        "link": "#",
+        "children": []
+      }
+    ],
+    "path": "/guide/using-vue.html",
+    "pathLocale": "/",
+    "extraFields": []
+  },
+  {
+    "title": "Introducere în curs",
+    "headers": [],
     "path": "/introduction/introduction.html",
     "pathLocale": "/",
     "extraFields": []
@@ -291,35 +320,6 @@ export const searchIndex = [
       }
     ],
     "path": "/introduction/program.html",
-    "pathLocale": "/",
-    "extraFields": []
-  },
-  {
-    "title": "Introducere în curs",
-    "headers": [],
-    "path": "/guide/",
-    "pathLocale": "/",
-    "extraFields": []
-  },
-  {
-    "title": "Using Vue in Markdown",
-    "headers": [
-      {
-        "level": 2,
-        "title": "Browser API Access Restrictions",
-        "slug": "browser-api-access-restrictions",
-        "link": "#browser-api-access-restrictions",
-        "children": []
-      },
-      {
-        "level": 2,
-        "title": "",
-        "slug": "",
-        "link": "#",
-        "children": []
-      }
-    ],
-    "path": "/guide/using-vue.html",
     "pathLocale": "/",
     "extraFields": []
   },
